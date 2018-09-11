fc708e29ef8c651486d632bfc8c2fb485e523ca5
Aleksei Riasnov <aleksei_riasnov@epam.com>
2018-09-11 11:29:15 +0400

    add jacoco dependency

--- src/test/java/ru/alexproject/blogserver/controllers/PostControllerTest.java
+++ src/test/java/ru/alexproject/blogserver/controllers/PostControllerTest.java
@@ -3,10 +3,13 @@ package ru.alexproject.blogserver.controllers;
 //import org.junit.Assert;
 //import org.junit.Test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PostControllerTest {
 
-  //  @Test
-  //  public void getAllPostsTest(){
-        //Assert.assertTrue(2 == 2);
-   // }
+    @Test
+    public void getAllPostsTest(){
+        Assert.assertTrue(2 == 2);
+    }
 }
